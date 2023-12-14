3244043aef70558e0736b90462c4c6c112503ba1
kimjh <scottgg12345@gmail.com>
2023-12-14 12:02:14 +0900

    nice5

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,5 +1,9 @@
 package com.ll.medium.domain.member.member.controller;
 
+
+import com.ll.medium.domain.member.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -7,27 +11,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
     }
-
     @Setter
     @Getter
     public static class JoinForm {
+        @NotBlank
         private String username;
+        @NotBlank
         private String password;
     }
 
     @PostMapping("/join")
-    public String signup(JoinForm joinForm) {
 
+        public String join(@Valid JoinForm joinForm) {
+            memberService.join(joinForm.getUsername(), joinForm.getPassword());
 
-        return "redirect:/";
+            return "redirect:/";
+        }
     }
-}
